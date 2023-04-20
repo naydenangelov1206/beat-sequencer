2b6dbc91055b3e79f8f8f55adee5f2c3e1437149
naydenangelov1206 <naydenangelov1206@gmail.com>
2023-04-20 17:48:48 +0300

    adding sounds and animation to the loop

--- app.js
+++ app.js
@@ -17,7 +17,27 @@ class DrumKit {
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    console.log(step);
+
+    activeBars.forEach(bar => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+
+      if (bar.classList.contains("active")) {
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
 
@@ -37,4 +57,7 @@ drumKit.playBtn.addEventListener("click", () => {
 
 drumKit.pads.forEach(pad => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
