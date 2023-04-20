9000a44b246543c3ac09c7adb360abb69d0f2528
naydenangelov1206 <naydenangelov1206@gmail.com>
2023-04-20 17:20:03 +0300

    making a loop for the drums

---
+++ app.js
@@ -0,0 +1,40 @@
+class DrumKit {
+  constructor() {
+    this.pads = document.querySelectorAll(".pad");
+    this.kickAudio = document.querySelector(".kick-sound");
+    this.snareAudio = document.querySelector(".snare-sound");
+    this.hihatAudio = document.querySelector(".hihat-sound");
+    this.playBtn = document.querySelector(".play");
+    this.bpm = 150;
+
+    this.index = 0;
+  }
+
+  activePad() {
+    this.classList.toggle("active");
+  }
+
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    console.log(step);
+    this.index++;
+  }
+
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
+}
+
+const drumKit = new DrumKit();
+
+drumKit.playBtn.addEventListener("click", () => {
+  drumKit.start();
+});
+
+drumKit.pads.forEach(pad => {
+  pad.addEventListener("click", drumKit.activePad);
+});
